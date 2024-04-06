1de1b4281bd3c5addb7dcf2a974f8301499c794e
ManickaJothiManimaran <165898125+ManickaJothiManimaran@users.noreply.github.com>
2024-04-07 01:14:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "environment" {
   source     = "./modules"
-  access_key = "AKIA2UC265NPUPH2DV4S"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
